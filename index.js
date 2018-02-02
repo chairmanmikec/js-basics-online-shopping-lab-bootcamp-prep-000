0cd8987e11e82e9a959ecafe40f74ad781a4826e
chairmanmikec <michaelc1012@gmail.com>
2018-02-02 21:34:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber === false){
-    
+  if (cardNumber === 0){
+    console.log("Sorry, we don't have a credit card on file for you.")
   }
 }
